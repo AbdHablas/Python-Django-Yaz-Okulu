62e43edfb4fd285566e0634a024a831f83141b16
AbdHablas <abd.hablas@gmail.com>
2020-07-27 23:53:00 +0300

    Home App Test view, urls Done

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    university = "Karabuk University"
+    dept = "Computer Engineering"
+    context = {'university': university, 'department': dept}
+
+    return render(request, 'index.html', context)
