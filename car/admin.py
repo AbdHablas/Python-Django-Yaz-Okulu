9cb16a8b38c4017d3c4195c03e2a45b3d94b6051
AbdHablas <abd.hablas@gmail.com>
2020-08-17 06:02:26 +0300

    Category SubCategory Menu

--- car/admin.py
+++ car/admin.py
@@ -1,5 +1,7 @@
 # Register your models here.
 from django.contrib import admin
+from django.utils.html import format_html
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
 
 from car.models import Category, Car, Image
 
@@ -14,19 +16,54 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ['status']
 
 
+class CategoryAdmin2(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_cars_count', 'related_cars_cumulative_count')
+    list_display_links = ('indented_title',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative car count
+        qs = Category.objects.add_related_count(
+            qs,
+            Car,
+            'category',
+            'cars_cumulative_count',
+            cumulative=True)
+
+        # Add non cumulative product count
+        qs = Category.objects.add_related_count(qs,
+                                                Car,
+                                                'category',
+                                                'cars_count',
+                                                cumulative=False)
+        return qs
+
+    def related_cars_count(self, instance):
+        return instance.cars_count
+        related_cars_count.short_description = 'Related products (for this specific category)'
+
+    def related_cars_cumulative_count(self, instance):
+        return instance.cars_cumulative_count
+
+    related_cars_cumulative_count.short_description = 'Related cars (in tree)'
+
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'rent_price', 'description', 'passenger_amount',
-                    'door_number', 'image_tag', 'Bag', 'transmission_type', 'miles', 'parent', 'status']
+                    'door_number', 'Bag', 'transmission_type', 'miles', 'parent', 'status']
     readonly_fields = ('image_tag',)
     list_filter = ['category', 'status']
     inlines = [CarImageInline]
 
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'Car', 'image_tag']
+    list_display = ['title', 'Car']
     readonly_fields = ('image_tag',)
 
 
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category, CategoryAdmin2)
 admin.site.register(Car, CarAdmin)
 admin.site.register(Image, ImageAdmin)
--- car/models.py
+++ car/models.py
@@ -3,9 +3,11 @@ from django.db import models
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     STATUS = (
         ('True', 'True'),
         ('False', 'False'),
@@ -16,7 +18,7 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=255, choices=STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -48,8 +50,17 @@ class Car(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.title
+    class MPTTMeta:
+        level_attr = 'mptt_level'
+        order_insertion_by = ['title']
+
+    def __str__(self):  # __str__ method elaborated later in
+        full_path = [self.title]  # post.  use __unicode__ in place of
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' / '.join(full_path[::-1])
 
     def image_tag(self):
         if self.image.url is not None:
