8dbb389c308952d413890897b6514716dc0819dd
AbdHablas <abd.hablas@gmail.com>
2020-08-18 03:27:15 +0300

    retrieving dynamically from - DBlisting Page

--- home/views.py
+++ home/views.py
@@ -13,37 +13,38 @@ def index(request):
     setting = Setting.objects.get(pk=1)
     sliderdata = Car.objects.all()[:1]
     categories = Category.objects.all()
-    cars=Car.objects.all()
-    context = {'setting': setting,'page': 'home','sliderdata':sliderdata,'categories':categories,'cars':cars}
+    cars = Car.objects.all()
+    context = {'setting': setting, 'page': 'home', 'sliderdata': sliderdata, 'categories': categories, 'cars': cars}
     return render(request, 'index.html', context)
+
 
 def ListCar(request):
     setting = Setting.objects.get(pk=1)
     sliderdata = Car.objects.all()[:1]
     categories = Category.objects.all()
-    cars=Car.objects.all()
-    context = {'setting': setting,'page': 'home','sliderdata':sliderdata,'categories':categories,'cars':cars}
+    cars = Car.objects.all()
+    context = {'setting': setting, 'page': 'home', 'sliderdata': sliderdata, 'categories': categories, 'cars': cars}
     return render(request, 'ListCar.html', context)
 
 
 def about(request):
     setting = Setting.objects.get(pk=1)
     categories = Category.objects.all()
-    context = {'setting': setting, 'page': 'about','categories':categories}
+    context = {'setting': setting, 'page': 'about', 'categories': categories}
     return render(request, 'about.html', context)
 
 
 def references(request):
     setting = Setting.objects.get(pk=1)
     categories = Category.objects.all()
-    context = {'setting': setting, 'page': 'references','categories':categories}
+    context = {'setting': setting, 'page': 'references', 'categories': categories}
     return render(request, 'references.html', context)
 
 
 def contactus(request):
     setting = Setting.objects.get(pk=1)
     categories = Category.objects.all()
-    context = {'setting': setting, 'page': 'contact','categories':categories}
+    context = {'setting': setting, 'page': 'contact', 'categories': categories}
     return render(request, 'contact.html', context)
 
 
@@ -60,7 +61,8 @@ def contact(request):
             data.message = form.cleaned_data['message']
             data.ip = request.META.get('REMOTE_ADDR')
             data.save()  # save data to table
-            context = {'setting': setting, 'page': 'contact','categories':categories,'messages':[{'success':"Your message has ben sent. Thank you for your message.","tag":'success'}]}
+            context = {'setting': setting, 'page': 'contact', 'categories': categories, 'messages': [
+                {'success': "Your message has ben sent. Thank you for your message.", "tag": 'success'}]}
             return render(request, 'contact.html', context)
-    context = {'setting': setting, 'page': 'contact','categories':categories}
+    context = {'setting': setting, 'page': 'contact', 'categories': categories}
     return render(request, 'contact.html', context)
